f1ba88b91d2eea069c13f1a514702957be6c5b89
Matthias Koch <ithrowexceptions@gmail.com>
2016-06-02 22:00:52 +0200

    Stubbed execution with grouping event.

--- src/TestLinker/Actions/GotoLinkedTypesAction.cs
+++ src/TestLinker/Actions/GotoLinkedTypesAction.cs
@@ -16,19 +16,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using JetBrains.ActionManagement;
+using JetBrains.Application;
 using JetBrains.Application.DataContext;
+using JetBrains.DataFlow;
 using JetBrains.ProjectModel;
 using JetBrains.ProjectModel.DataContext;
 using JetBrains.ReSharper.Feature.Services.Actions;
+using JetBrains.ReSharper.Feature.Services.Navigation.ExecutionHosting;
 using JetBrains.ReSharper.Feature.Services.Occurences;
 using JetBrains.ReSharper.Feature.Services.Tree;
 using JetBrains.ReSharper.Feature.Services.Util;
 using JetBrains.ReSharper.Psi;
 using JetBrains.ReSharper.Psi.Tree;
-using JetBrains.ReSharper.Resources.Shell;
 using JetBrains.TextControl;
 using JetBrains.TextControl.DataContext;
+using JetBrains.Threading;
 using JetBrains.UI.ActionsRevised;
+using JetBrains.UI.DataContext;
 using JetBrains.UI.PopupWindowManager;
 using JetBrains.Util;
 using TestLinker.Navigation;
@@ -38,10 +42,18 @@ namespace TestLinker.Actions
   [Action ("Goto_LinkedTypes", "Goto Linked Types", Id = 9854)]
   public class GotoLinkedTypesAction : IActionWithExecuteRequirement, IExecutableAction
   {
+    private GroupingEvent _executionGroupingEvent;
+
+    private ISolution _solution;
+    private ITextControl _textControl;
+    private LinkedTypesService _linkedTypesProvider;
+    private PopupWindowContextSource _popupWindowContextSource;
+
     #region IActionWithExecuteRequirement
 
     public IActionRequirement GetRequirement (IDataContext dataContext)
     {
+      _executionGroupingEvent = _executionGroupingEvent ?? CreateExecuteGroupingEvent(dataContext);
       return CurrentPsiFileRequirement.FromDataContext(dataContext);
     }
 
@@ -57,31 +69,46 @@ namespace TestLinker.Actions
 
     public void Execute (IDataContext context, DelegateExecute nextExecute)
     {
-      var solution = context.GetData(ProjectModelDataConstants.SOLUTION).NotNull();
-      var textControl = context.GetData(TextControlDataConstants.TEXT_CONTROL).NotNull();
-      var typesInContext = GetTypesInContext(textControl, solution).ToList();
+      _solution = context.GetData(ProjectModelDataConstants.SOLUTION).NotNull();
+      _textControl = context.GetData(TextControlDataConstants.TEXT_CONTROL).NotNull();
+      _linkedTypesProvider = _solution.GetComponent<LinkedTypesService>();
+      _popupWindowContextSource = context.GetData(UIDataConstants.PopupWindowContextSource);
 
-      var linkedTypesProvider = solution.GetComponent<LinkedTypesService>();
-      var linkedTypes = typesInContext.SelectMany(x => linkedTypesProvider.GetLinkedTypes(x)).ToHashSet();
+      //_executionGroupingEvent.FireIncoming();
+      ExecuteProlongated();
+    }
+
+    #endregion
+
+    #region Privates
+
+    private void ExecuteProlongated ()
+    {
+      var typesInContext = GetTypesInContext(_textControl, _solution).ToList();
+      var linkedTypes = typesInContext.SelectMany(x => _linkedTypesProvider.GetLinkedTypes(x)).ToHashSet();
       if (linkedTypes.IsEmpty())
         return;
 
       var occurrences = linkedTypes.Select(x => new LinkedTypesOccurrence(x, OccurenceType.Occurence)).ToList<IOccurence>();
       if (occurrences.Count == 1)
       {
-        occurrences.Single().Navigate(solution, Shell.Instance.GetComponent<MainWindowPopupWindowContext>().Source, true);
+        occurrences.Single().Navigate(_solution, _popupWindowContextSource, transferFocus: true);
       }
       else
       {
-        Func<OccurenceBrowserDescriptor> descriptorBuilder = () => new LinkedTypesOccurrenceBrowserDescriptor(solution, typesInContext, occurrences);
-        var popup = context.GetComponent<OccurencePopupMenu>();
-        popup.ShowMenuFromTextControl(context, occurrences, descriptorBuilder, new OccurencePresentationOptions(), true, "Go to linked types ");
+        Func<OccurenceBrowserDescriptor> descriptorBuilder = () => new LinkedTypesOccurrenceBrowserDescriptor(_solution, typesInContext, occurrences);
+        var navigationExecutionHost = _solution.GetComponent<DefaultNavigationExecutionHost>();
+        navigationExecutionHost.ShowGlobalPopupMenu(
+            _solution,
+            occurrences,
+            activate: true,
+            windowContext: _popupWindowContextSource, 
+            descriptorBuilder: descriptorBuilder,
+            options: new OccurencePresentationOptions(),
+            skipMenuIfSingleEnabled: true,
+            title: "Go to linked types ");
       }
     }
-
-    #endregion
-
-    #region Privates
 
     private IEnumerable<ITypeElement> GetTypesInContext (ITextControl textControl, ISolution solution)
     {
@@ -102,6 +129,17 @@ namespace TestLinker.Actions
         return symbolCache.GetTypesAndNamespacesInFile(psiSourceFile).OfType<ITypeElement>();
 
       return Enumerable.Empty<ITypeElement>();
+    }
+
+    private GroupingEvent CreateExecuteGroupingEvent (IDataContext dataContext)
+    {
+      var shellLocks = dataContext.GetComponent<IShellLocks>();
+      return shellLocks.GroupingEvents.CreateEvent(
+          Lifetimes.Define().Lifetime,
+          "GotoLinkedTypes",
+          TimeSpan.FromMilliseconds(value: 1000),
+          Rgc.Guarded,
+          ExecuteProlongated);
     }
 
     #endregion

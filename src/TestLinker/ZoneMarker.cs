49ed57e701ed92a07325c255558245fc4a45e9ab
Matthias Koch <ithrowexceptions@gmail.com>
2016-06-04 00:47:21 +0200

    Improved zone marker.

--- src/TestLinker/ZoneMarker.cs
+++ src/TestLinker/ZoneMarker.cs
@@ -18,11 +18,17 @@ using JetBrains.Application.BuildScript.Application.Zones;
 using JetBrains.ReSharper.Feature.Services.Daemon;
 using JetBrains.ReSharper.Psi.CSharp;
 using JetBrains.ReSharper.UnitTestFramework;
+using JetBrains.TextControl;
 
 namespace TestLinker
 {
   [ZoneMarker]
-  public class ZoneMarker : IRequire<IUnitTestingZone>, IRequire<ILanguageCSharpZone>, IRequire<DaemonEngineZone>
+  public class ZoneMarker
+      :
+          IRequire<IUnitTestingZone>,
+          IRequire<ILanguageCSharpZone>,
+          IRequire<DaemonEngineZone>,
+          IRequire<ITextControlsZone>
   {
   }
 }

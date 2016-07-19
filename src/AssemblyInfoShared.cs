3236f7f245b9960aa2d8a460c051b7eba7e129ab
Matthias Koch <ithrowexceptions@gmail.com>
2016-07-19 04:56:39 +0200

    Fixed AssemblyProduct.

--- src/AssemblyInfoShared.cs
+++ src/AssemblyInfoShared.cs
@@ -16,7 +16,7 @@ using System;
 using System.Reflection;
 
 [assembly: AssemblyCompany ("Matthias Koch")]
-[assembly: AssemblyProduct ("TestFx.TestLinker")]
+[assembly: AssemblyProduct ("TestLinker.ReSharper")]
 [assembly: AssemblyDescription("Linking subject entities and tests through attributes.")]
 [assembly: AssemblyCopyright ("Copyright © Matthias Koch, 2016 - Licensed under APACHE 2.0")]
 [assembly: AssemblyTrademark ("")]
